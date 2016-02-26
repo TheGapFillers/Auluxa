43d769d27ecc1f9c5fac6954f1739243758f7db5
Boris Roux <boris.roux@gmail.com>
2016-02-26 21:21:11 +0800

    Add more unit tests

--- Auluxa/Auluxa.WebApp.Tests/ApplianceModelControllerTest.cs
+++ Auluxa/Auluxa.WebApp.Tests/ApplianceModelControllerTest.cs
@@ -1,14 +1,10 @@
 ﻿using Auluxa.WebApp.Controllers;
 using Auluxa.WebApp.Models;
 using Auluxa.WebApp.Repositories;
-using Auluxa.WebApp.Tests.Repositories;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using System.Web.Http;
 using System.Web.Http.Results;
 
 namespace Auluxa.WebApp.Tests
@@ -17,24 +13,24 @@ namespace Auluxa.WebApp.Tests
 	public class ApplianceModelControllerTest
 	{
 		[Test]
-		public async Task GetApplianceModelTest()
+		public async Task ApplianceModel_GetTest()
 		{
 			var context = new TestDbContext();
-			context.ApplianceModels.Add(BuildTestApplianceModel());
+			ApplianceModel am = BuildTestApplianceModel();
+			context.ApplianceModels.Add(am);
 
 			var applianceModelController = new ApplianceModelController(new EfApplicationRepository { Context = context });
 			applianceModelController.Request = new System.Net.Http.HttpRequestMessage { RequestUri = new Uri("http://localhost:57776/api/models") };
 
-			var result = await applianceModelController.Get() as OkNegotiatedContentResult<ApplianceModel>;
+			var result = await applianceModelController.Get() as OkNegotiatedContentResult<List<ApplianceModel>>;
 
 			Assert.IsNotNull(result);
-			//Assert.AreEqual(result.Content.Category, am.Category);
-			//Assert.AreEqual(result.Content.BrandName, am.BrandName);
-			//Assert.AreEqual(result.Content.ModelName, am.ModelName);
+			Assert.AreEqual(1, result.Content.Count);
+			AssertApplianceModelsAreEqual(result.Content[0], am);
 		}
 
 		[Test]
-		public async Task PostApplianceModelTest()
+		public async Task ApplianceModel_PostTest()
 		{
 			var applianceModelController = new ApplianceModelController(new EfApplicationRepository { Context = new TestDbContext()});
 			applianceModelController.Request = new System.Net.Http.HttpRequestMessage { RequestUri = new Uri("http://localhost:57776/api/models") };
@@ -44,9 +40,58 @@ namespace Auluxa.WebApp.Tests
 			var result = await applianceModelController.Post(am) as CreatedNegotiatedContentResult<ApplianceModel>;
 
 			Assert.IsNotNull(result);
-			Assert.AreEqual(result.Content.Category, am.Category);
-			Assert.AreEqual(result.Content.BrandName, am.BrandName);
-			Assert.AreEqual(result.Content.ModelName, am.ModelName);
+			AssertApplianceModelsAreEqual(result.Content, am);
+		}
+
+		[Test]
+		public async Task ApplianceModel_PatchTest()
+		{
+			var context = new TestDbContext();
+			ApplianceModel am = BuildTestApplianceModel();
+			context.ApplianceModels.Add(am);
+
+			var applianceModelController = new ApplianceModelController(new EfApplicationRepository { Context = context });
+			applianceModelController.Request = new System.Net.Http.HttpRequestMessage { RequestUri = new Uri("http://localhost:57776/api/models") };
+
+			// Modify the appliance, send patch and check result
+			//todo test null parameters
+			am.Category = "SuperAC";
+			am.BrandName = "Midea";
+			am.ModelName = "AC3000";
+			var resultPatch = await applianceModelController.Patch(am) as CreatedNegotiatedContentResult<ApplianceModel>;
+
+			Assert.IsNotNull(resultPatch);
+			AssertApplianceModelsAreEqual(resultPatch.Content, am);
+
+			// Get all appliances must return modified appliance
+			var resultGet = await applianceModelController.Get() as OkNegotiatedContentResult<List<ApplianceModel>>;
+
+			Assert.IsNotNull(resultGet);
+			Assert.AreEqual(1, resultGet.Content.Count);
+			AssertApplianceModelsAreEqual(resultGet.Content[0], am);
+		}
+
+		[Test]
+		public async Task ApplianceModel_DeleteTest()
+		{
+			var context = new TestDbContext();
+			ApplianceModel am = BuildTestApplianceModel();
+			context.ApplianceModels.Add(am);
+
+			var applianceModelController = new ApplianceModelController(new EfApplicationRepository { Context = context });
+			applianceModelController.Request = new System.Net.Http.HttpRequestMessage { RequestUri = new Uri("http://localhost:57776/api/models") };
+
+			// Delete the appliance, send command and check result
+			var resultDelete = await applianceModelController.Delete(am.Id) as OkNegotiatedContentResult<ApplianceModel>;
+
+			Assert.IsNotNull(resultDelete);
+			AssertApplianceModelsAreEqual(resultDelete.Content, am);
+
+			// Get all appliances must return empty set
+			var resultGet = await applianceModelController.Get() as OkNegotiatedContentResult<List<ApplianceModel>>;
+
+			Assert.IsNotNull(resultGet);
+			Assert.AreEqual(0, resultGet.Content.Count);
 		}
 
 		private ApplianceModel BuildTestApplianceModel()
@@ -55,6 +100,13 @@ namespace Auluxa.WebApp.Tests
 			{
 				Id = 0, Category = "AC", BrandName = "Dyson", ModelName = "AC2000"
 			};
+		}
+
+		private void AssertApplianceModelsAreEqual(ApplianceModel expected, ApplianceModel actual)
+		{
+			Assert.AreEqual(expected.Category, actual.Category);
+			Assert.AreEqual(expected.BrandName, actual.BrandName);
+			Assert.AreEqual(expected.ModelName, actual.ModelName);
 		}
 	}
 }
--- Auluxa/Auluxa.WebApp.Tests/ZoneControllerTest.cs
+++ Auluxa/Auluxa.WebApp.Tests/ZoneControllerTest.cs
@@ -1,12 +1,9 @@
 ﻿using Auluxa.WebApp.Controllers;
 using Auluxa.WebApp.Models;
 using Auluxa.WebApp.Repositories;
-using Auluxa.WebApp.Tests.Repositories;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http.Results;
 
@@ -16,29 +13,142 @@ namespace Auluxa.WebApp.Tests
 	public class ZoneControllerTest
 	{
 		[Test]
-		public async Task PostZoneTest()
+		public async Task Zone_GetTest()
 		{
-			var zoneController = new ZoneController(new EfApplicationRepository { Context = new TestDbContext()});
-			Zone zone = GetTestZone();
+			var context = new TestDbContext();
+			Zone z = BuildTestZoneModel();
+			context.Zones.Add(z);
 
-			var result = await zoneController.Post(zone) as CreatedAtRouteNegotiatedContentResult<Zone>;
+			var zonesController = new ZoneController(new EfApplicationRepository { Context = context });
+			zonesController.Request = new System.Net.Http.HttpRequestMessage { RequestUri = new Uri("http://localhost:57776/api/models") };
+
+			var result = await zonesController.Get() as OkNegotiatedContentResult<List<Zone>>;
 
 			Assert.IsNotNull(result);
-			Assert.AreEqual(result.RouteName, "DefaultApi");
-			Assert.AreEqual(result.RouteValues["id"], result.Content.Id);
-			Assert.AreEqual(result.Content.Name, zone.Name);
+			Assert.AreEqual(1, result.Content.Count);
+			AssertZonesAreEqual(result.Content[0], z);
 		}
 
-		private Zone GetTestZone()
+		[Test]
+		public async Task Zone_PostTest()
+		{
+			var zonesController = new ZoneController(new EfApplicationRepository { Context = new TestDbContext() });
+			zonesController.Request = new System.Net.Http.HttpRequestMessage { RequestUri = new Uri("http://localhost:57776/api/models") };
+
+			Zone z = BuildTestZoneModel();
+
+			var result = await zonesController.Post(z) as CreatedNegotiatedContentResult<Zone>;
+
+			Assert.IsNotNull(result);
+			AssertZonesAreEqual(result.Content, z);
+		}
+
+		[Test]
+		public async Task Zone_PatchTest()
+		{
+			var context = new TestDbContext();
+			Zone z = BuildTestZoneModel();
+			context.Zones.Add(z);
+
+			var zonesController = new ZoneController(new EfApplicationRepository { Context = context });
+			zonesController.Request = new System.Net.Http.HttpRequestMessage { RequestUri = new Uri("http://localhost:57776/api/models") };
+
+			// Modify the appliance, send patch and check result
+			//todo test null parameters
+			z.Name = "Bermuda Triangle";
+			z.UserName = "Jack Sparrow";
+			z.Appliances = new List<Appliance>
+			{
+				new Appliance
+				{
+					Id = 1,
+					Name = "The Black Rock",
+					Model = new ApplianceModel()
+					{
+						Id = 0,
+						Category = "Sea Ship",
+						BrandName = "Unknown",
+						ModelName = "Black Rock"
+					},
+					CurrentSetting = new Dictionary<string, string>
+					{
+						["MaxSpeed"] = "60",
+						["Cannons"] = "30"
+					}
+				},
+			};
+			var resultPatch = await zonesController.Patch(z) as CreatedNegotiatedContentResult<Zone>;
+
+			Assert.IsNotNull(resultPatch);
+			AssertZonesAreEqual(resultPatch.Content, z);
+
+			// Get all appliances must return modified appliance
+			var resultGet = await zonesController.Get() as OkNegotiatedContentResult<List<Zone>>;
+
+			Assert.IsNotNull(resultGet);
+			Assert.AreEqual(1, resultGet.Content.Count);
+			AssertZonesAreEqual(resultGet.Content[0], z);
+		}
+
+		[Test]
+		public async Task ApplianceModel_DeleteTest()
+		{
+			var context = new TestDbContext();
+			Zone z = BuildTestZoneModel();
+			context.Zones.Add(z);
+
+			var zonesController = new ZoneController(new EfApplicationRepository { Context = context });
+			zonesController.Request = new System.Net.Http.HttpRequestMessage { RequestUri = new Uri("http://localhost:57776/api/models") };
+
+			// Delete the appliance, send command and check result
+			var resultDelete = await zonesController.Delete(z.Id) as OkNegotiatedContentResult<Zone>;
+
+			Assert.IsNotNull(resultDelete);
+			AssertZonesAreEqual(resultDelete.Content, z);
+
+			// Get all appliances must return empty set
+			//todo should we make sure related appliances are not deleted in the process?
+			var resultGet = await zonesController.Get() as OkNegotiatedContentResult<List<Zone>>;
+
+			Assert.IsNotNull(resultGet);
+			Assert.AreEqual(0, resultGet.Content.Count);
+		}
+
+		private Zone BuildTestZoneModel()
 		{
 			return new Zone()
 			{
-				Id = 1, Name = "Test Area", UserName = "Mr Tester",
+				Id = 0,
+				Name = "Area 51",
+				UserName = "Agent K",
 				Appliances = new List<Appliance>
 				{
-					new Appliance { Id = 0, Name = "AC" }
+					new Appliance
+					{
+						Id = 1,
+						Name = "Millenium Falcon",
+						Model = new ApplianceModel()
+						{
+							Id = 0,
+							Category = "Space Ship",
+							BrandName = "Corellian Engineering Corporation",
+							ModelName = "YT-1300"
+						},
+						CurrentSetting = new Dictionary<string, string>
+						{
+							["HyperSpaceSpeed"] = "42",
+							["CannonTowers"] = "2"
+						}
+					},
 				}
 			};
+		}
+
+		private void AssertZonesAreEqual(Zone expected, Zone actual)
+		{
+			Assert.AreEqual(expected.Name, actual.Name);
+			Assert.AreEqual(expected.UserName, actual.UserName);
+			Assert.AreEqual(expected.Appliances, actual.Appliances);
 		}
 	}
 }
