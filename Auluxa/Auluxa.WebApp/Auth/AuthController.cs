31ca9e10e1513eaaaf743c6fd1ff615548825df9
Ambroise Couissin <ambroise.couissin@gmail.com>
2016-03-08 19:06:45 +0800

    SubscriptionController out of auth

--- Auluxa/Auluxa.WebApp/Auth/AuthController.cs
+++ Auluxa/Auluxa.WebApp/Auth/AuthController.cs
@@ -10,12 +10,10 @@ namespace Auluxa.WebApp.Auth
     public class AuthController : ApiController
     {
         private readonly AuthUserManager _userManager;
-        private readonly ISubscriptionRepository _subscriptuionRepository;
 
-        public AuthController(AuthUserManager userManager, ISubscriptionRepository subscriptionRepository)
+        public AuthController(AuthUserManager userManager)
         {
             _userManager = userManager;
-            _subscriptuionRepository = subscriptionRepository;
         }
 
         [AllowAnonymous]
@@ -29,36 +27,6 @@ namespace Auluxa.WebApp.Auth
                 return Ok(result);
 
             foreach (string error in result.Errors)
-                ModelState.AddModelError(error, error);
-            return BadRequest(ModelState);
-        }
-
-        [HttpPost]
-        [Route("subscribe")]
-        public async Task<IHttpActionResult> SubscribeToPlan(
-            string email,
-            [FromBody] Subscription.Subscription subscription)
-        {
-            // Get the user
-            AuthUser user = await _userManager.FindByEmailAsync(email);
-            if (user == null)
-            {
-                ModelState.AddModelError("user", "This email has not been registered before.");
-                return BadRequest(ModelState);
-            }
-
-            // subscribe the user to a new plan
-            Subscription.Subscription createdSubscription = await _subscriptuionRepository.SubscribeToPlanAsync(
-                user, subscription);
-
-            // update the user repository
-            user.ChargeBeeSubscriptionId = createdSubscription.Id;
-            user.SubscriptionType = createdSubscription.SubscriptionType;
-            IdentityResult updateResult = await _userManager.UpdateAsync(user);
-            if (updateResult.Succeeded)
-                return Ok(subscription);
-
-            foreach (string error in updateResult.Errors)
                 ModelState.AddModelError(error, error);
             return BadRequest(ModelState);
         }
---
+++ Auluxa/Auluxa.WebApp/Subscription/SubscriptionController.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+using Auluxa.WebApp.Auth;
+using Microsoft.AspNet.Identity;
+
+namespace Auluxa.WebApp.Subscription
+{
+    [Authorize]
+    [RoutePrefix("subscription")]
+    public class SubscriptionController : ApiController
+    {
+        private readonly AuthUserManager _userManager;
+        private readonly ISubscriptionRepository _subscriptionRepository;
+
+        public SubscriptionController(AuthUserManager userManager, ISubscriptionRepository subscriptionRepository)
+        {
+            _userManager = userManager;
+            _subscriptionRepository = subscriptionRepository;
+        }
+
+        [HttpPost]
+        [Route]
+        public async Task<IHttpActionResult> SubscribeToPlan(string email, [FromBody]Subscription subscription)
+        {
+            // Get the user
+            AuthUser user = await _userManager.FindByEmailAsync(email);
+            if (user == null)
+            {
+                ModelState.AddModelError("user", "This email has not been registered before.");
+                return BadRequest(ModelState);
+            }
+
+            // subscribe the user to a new plan
+            Subscription createdSubscription = await _subscriptionRepository.SubscribeToPlanAsync(
+                user, subscription);
+
+            // update the user repository
+            user.ChargeBeeSubscriptionId = createdSubscription.Id;
+            user.SubscriptionType = createdSubscription.SubscriptionType;
+            IdentityResult updateResult = await _userManager.UpdateAsync(user);
+            if (updateResult.Succeeded)
+                return Ok(subscription);
+
+            foreach (string error in updateResult.Errors)
+                ModelState.AddModelError(error, error);
+            return BadRequest(ModelState);
+        }
+    }
+}
